66a7861a6af43ffe0ba91805cc74062540956be9
KDT114 <jiskain794@naver.com>
2023-07-24 20:11:33 +0900

    기본적인틀 만들기2

--- class_client/class_client_1.py
+++ class_client/class_client_1.py
@@ -1,111 +1,95 @@
-import sqlite3
-import sys
-from socket import *
 from threading import *
+from socket import *
+from temporary_storage import Store
 
-from roll_client import Client
-from fun_login import LogIn
-from main_display import MainDisplay
-from func_chat import ChatDialog
-from class_blue import ClassBlueInfo
-from admin_chat import AdminChat
-from pw_dialog import PwChange
-from car_dialog import CarChange
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+# _SERVER_IP = '10.10.20.109'
+_SERVER_IP = gethostbyname(gethostname())
+_SERVER_PORT = 5050
+BUFFER = 50000
+FORMAT = "utf-8"
+_CONNECT = (_SERVER_IP, _SERVER_PORT)
 
 header_split = chr(1)
 list_split_1 = chr(2)
 list_split_2 = chr(3)
-BUFFER = 50000
-FORMAT = "utf-8"
 
-class Controller(QWidget):
-    JoinSignal = pyqtSignal(str)
-    ChangeSignal = pyqtSignal(bool)
-    LoginSignal = pyqtSignal(bool)
-    ChatOpenSignal = pyqtSignal(str)
-    def __init__(self):
+
+class Client(Store):
+    def __init__(self, ui_controller = None):
         super().__init__()
-        self.duple_check = None
-        self.client_object = Client(self)
-        self.login_object = LogIn(self)
-        self.main_object = MainDisplay(self)
-        self.chat_object = ChatDialog(self)
-        self.blue_object = ClassBlueInfo(controller = self)
-        self.admin_object = AdminChat(self)
-        self.pwchange_object = PwChange(self)
-        self.carchange_object = CarChange(self)
-        self.init_var()
-        self.init_ui()
-        self.init_func()
+        self.ui_controller = ui_controller
+        self.client_socket = None
+        self._connected = None
 
+        self.connect_server()
+        self.listeningThread = Thread(target=self.check_server_response, daemon=True)
+        self.listeningThread.start()
 
-    def init_ui(self):
-        pass
-
-    def init_func(self):
-        self.JoinSignal.connect(self.emit_login_object)
-        self.ChangeSignal.connect(self.emit_login_object_duple_status)
-
-    def init_var(self):
-        pass
+    def connect_server(self):
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect(_CONNECT)
+        message = f"{f'enter{header_split}접속한다':{BUFFER}}".encode(FORMAT)
+        self.send_message(message)
+        self._connected = True
 
     def send_message(self, message):
-        print(message)
-        self.client_object.send_message(message)
+        self.client_socket.send(message)
 
-    def emit_login_object(self, message):
-        self.login_object.AlertSignal.emit(message)
+    def check_server_response(self):
+        while self._connected:
+            # if not self.client_socket == None:
+            try:
+                response = self.client_socket.recv(BUFFER).decode(FORMAT).strip()
+                print("메인 레스폰스", response)
+                self._parse_packet(response)
 
-    def emit_login_object_duple_status(self):
-        self.login_object.ChangeDuple.emit(True)
+            except Exception as e:
+                print(e)
+                pass
 
-    def emit_login_object_login(self, result):
-        self.login_object.LoginSignal.emit(result)
+    def _parse_packet(self, p: str):
+        parsed = p.split(header_split)
+        header = parsed[0].strip()
 
-    def emit_add_msg(self, target, txt):
-        """
-        target is admin or member
-        """
-        self.chat_object.AddMsg.emit(target, txt)
-        self.admin_object.AddMsg.emit(target, txt)
-
-    def open_main_display(self):
-        self.main_object.show()
-
-    def emit_blue_info(self, data):
-        self.main_object.blueSignal.emit(data)
-
-    def emit_set_center_name(self, name):
-        self.main_object.CenterNameSignal.emit(name)
-
-    def emit_alert_reserve_duple(self, message):
-        self.main_object.AlertReserveDuple.emit(message)
-
-    def emit_change_status_reserve(self):
-        self.main_object.StatusReserve.emit()
-
-    def emit_set_reserve_check_page(self, list_):
-        self.main_object.ReserveCheck.emit(list_)
-
-    def emit_check_reserve_duple(self, result):
-        self.main_object.CheckReserveDuple.emit(result)
-
-    def emit_mypage_data(self):
-        self.main_object.MypageSignal.emit()
-
-    def emit_open_pw_change(self):
-        self.pwchange_object.exec()
-
-    def emit_open_car_change(self):
-        self.carchange_object.exec()
-
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mywin = Controller()
-    mywin.login_object.show()
-    app.exec()
+        if header == 'duple': # 아이디 중복 검사 true면 노중복 false면 중복
+            result = parsed[1]
+            if result == 'true':
+                self.ui_controller.emit_login_object("사용 가능한 아이디 입니다")
+                self.ui_controller.emit_login_object_duple_status()
+            elif result == 'false':
+                self.ui_controller.emit_login_object("사용할 수 없는 아이디입니다")
+        elif header == 'login':
+            result = parsed[1]
+            if result == 'true':
+                self.ui_controller.emit_login_object_login('true')
+            elif result == 'false':
+                self.ui_controller.emit_login_object_login('false')
+        elif header == 'info':
+            substance = parsed[1].split(list_split_1)
+            self.member_info['id'] = [substance[0]]
+            self.member_info['pw'] = [substance[1]]
+            self.member_info['name'] = [substance[2]]
+            self.member_info['car'] = [substance[3]]
+            self.ui_controller.emit_mypage_data()
+        elif header == 'open':
+            self.ui_controller.emit_add_msg(f'{header}','무엇을 도와드릴까요?')
+        elif header == 'member':
+            msg = parsed[1]
+            self.ui_controller.emit_add_msg(f'{header}', msg)
+        elif header == 'admin':
+            msg = parsed[1]
+            self.ui_controller.emit_add_msg(f'{header}', msg)
+        elif header == 'blue':
+            substance = parsed[1]
+            data = substance.split(list_split_1)
+            self.ui_controller.emit_blue_info(data)
+        elif header == 'r_duple':
+            result = parsed[1]
+            self.ui_controller.emit_alert_reserve_duple(result)
+        elif header == 'req_reserve_data':
+            substance = parsed[1]
+            receive_data = substance.split(f"{list_split_1}")
+            self.ui_controller.emit_set_reserve_check_page(receive_data)
+        elif header == 'reserve_duple':
+            receive_data = parsed[1]
+            self.ui_controller.emit_check_reserve_duple(receive_data)
